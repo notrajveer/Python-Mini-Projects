2a12f591d08446b214b9580022c5b417b6b7ab05
notrajveer <raia79898@gmail.com>
2026-01-07 21:09:36 +0530

    Add word counter mini project

---
+++ Word Counter/counter.py
@@ -0,0 +1,13 @@
+def count():
+    print ("Welcome to word counter!")
+    text = input("Enter your text here: ")
+    
+    words = text.split()
+    word_count = len(words)
+    char_count = len(text)
+    
+    print (f"\n Total words: {word_count}")
+    print (f"\n Total characters: {char_count}")
+    
+if __name__ == "__main__":
+    count()
